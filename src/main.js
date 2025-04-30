39972a0a250d865fdc424ac205454b190d98d634
ivantelizhenko <telizenkoo@gmail.com>
2025-04-30 16:03:40 +0300

    add mobile menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+// Show Dialog
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#mobile--header__button");
+const closeButton = document.querySelector("#close-dialog");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+  dialog.style.display = "grid";
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+  dialog.style.display = "none";
+});
