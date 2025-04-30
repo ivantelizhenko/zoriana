3671dd29f342927c43f502f44c27a0693a7aae3a
ivantelizhenko <telizenkoo@gmail.com>
2025-04-30 18:09:12 +0300

    зроблено header, dialog follow, dialog menu

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
-// Show Dialog
+"use strict";
 
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#mobile--header__button");
-const closeButton = document.querySelector("#close-dialog");
+// Show Dialog Menu
+const dialogMenu = document.querySelector(".dialog-menu");
+const openDialogMenuButton = document.querySelector("#open-dialog-menu");
+const closeDialogMenuButton = document.querySelector("#close-dialog-menu");
 
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-  dialog.style.display = "grid";
+openDialogMenuButton.addEventListener("click", () => {
+  dialogMenu.showModal();
+  dialogMenu.style.display = "grid";
 });
 
-closeButton.addEventListener("click", () => {
-  dialog.close();
-  dialog.style.display = "none";
+closeDialogMenuButton.addEventListener("click", () => {
+  dialogMenu.close();
+  dialogMenu.style.display = "none";
+});
+
+// Show Dialog Follow
+const dialogFollow = document.querySelector(".dialog-follow");
+const openDialogFollowButton = document.querySelectorAll(".open-dialog-follow");
+const closeDialogFollowButton = document.querySelector("#close-dialog-follow");
+
+console.log(dialogFollow);
+console.log(dialogMenu);
+
+openDialogFollowButton.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (dialogMenu.open) {
+      setTimeout(() => dialogMenu.close(), 0);
+      dialogMenu.style.display = "none";
+    }
+    dialogFollow.showModal();
+    dialogFollow.style.display = "grid";
+  })
+);
+
+closeDialogFollowButton.addEventListener("click", () => {
+  dialogFollow.close();
+  dialogFollow.style.display = "none";
 });
